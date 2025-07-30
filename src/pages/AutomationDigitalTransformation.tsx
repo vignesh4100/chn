d4a2d75f39acdb14d9ec342de6eb9cf0169d164f
Vkee-dev <vkee4100@gmail.com>
2025-07-30 20:43:24 +0530

    Updated AutomationDigitalTransformation.tsx

--- src/pages/AutomationDigitalTransformation.tsx
+++ src/pages/AutomationDigitalTransformation.tsx
@@ -329,7 +329,7 @@ const AutomationDigitalTransformation: React.FC = () => {
               
               <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-white hero-heading-large mb-6">
                 <span className="block">Automation &</span>
-                <span className="block text-transparent bg-clip-text bg-gradient-to-r from-purple-300 via-pink-300 to-cyan-300 animate-gradient-x">
+                <span className="block text-transparent bg-clip-text bg-gradient-to-r pb-2 from-purple-300 via-pink-300 to-cyan-300 animate-gradient-x">
                   Digital Transformation
                 </span>
               </h1>
